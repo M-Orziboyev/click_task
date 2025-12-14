c3d9f5c27b9a1a4432fa45fe2eb860a50f71e94c
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-12-14 23:31:00 +0500

    finally done

---
+++ src/pages/Index.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import Cart from '../components/cart/Cart';
+import { initializeProducts } from '../vanilla/products';
+
+const Index = () => {
+    useEffect(() => {
+        // Initialize vanilla JS products after component mounts
+        initializeProducts('products-container');
+    }, []);
+
+    return (
+        <div className="app-container">
+            <header className="app-header">
+                <h1>Mini Marketplace</h1>
+                <p>Discover amazing products at great prices</p>
+            </header>
+
+            <main className="main-layout">
+                {/* Vanilla JS Products Section - rendered outside React */}
+                <div id="products-container"></div>
+
+                {/* React Cart Component */}
+                <Cart />
+            </main>
+        </div>
+    );
+};
+
+export default Index;
